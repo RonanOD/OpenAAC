68e459c17ddcf3d4c63b9888f6093f4335e8f349
ronanwp <ronan@wattpad.com>
2023-12-23 16:36:38 -0400

    Successful upload to pinecone

--- db/cli/bin/cli.dart
+++ db/cli/bin/cli.dart
@@ -1,7 +1,8 @@
 import 'package:cli/cli.dart' as cli;
-import 'dart:io' show Platform;
+import 'dart:io' show Platform, sleep;
 import 'package:langchain_openai/langchain_openai.dart' show OpenAIEmbeddings;
 import 'package:langchain/langchain.dart' show Document;
+import 'package:pinecone/pinecone.dart';
 
 // The main() function is the entry point of the application
 // Run the application with: dart bin/cli.dart <path>
@@ -11,46 +12,81 @@ void main(List<String> arguments) {
     print('Usage: cli <path>');
     return;
   }
-  
+
   print('Utility to load images in ${arguments[0]} to embeddings database');
   List<String> images = cli.getImages(arguments[0]);
 
-  // Get OPENAI_API_KEY from environment variable
+  // Initialize variables
+  final modelName = 'text-embedding-ada-002';
+  final pcIndex = 'openaac-embeddings';
+
+  // Get OPENAI_API_KEY from environment variable: https://help.openai.com/en/articles/4936850-where-do-i-find-my-api-key
   final openAIApiKey = Platform.environment['OPENAI_API_KEY'];
   if (openAIApiKey == null) {
     print('OPENAI_API_KEY environment variable not set');
     return;
   }
 
-  // Get PINECONE_API_KEY from environment variable
+  // Get PINECONE_API_KEY from environment variable: https://docs.pinecone.io/docs/projects#api-keys
   final pineconeApiKey = Platform.environment['PINECONE_API_KEY'];
   if (pineconeApiKey == null) {
     print('PINECONE_API_KEY environment variable not set');
     return;
   }
 
-  // Initialize OpenAIEmbeddings
-  final modelName = 'text-embedding-ada-002';
+  // Get PINECONE_ENV from environment variable: https://docs.pinecone.io/docs/projects#project-environment
+  final pineconeEnv = Platform.environment['PINECONE_ENV'];
+  if (pineconeEnv == null) {
+    print('PINECONE_ENV environment variable not set');
+    return;
+  }
+
+  // Get PINECONE_PROJECT_ID from environment variable: https://docs.pinecone.io/docs/projects#project-id
+  final pineconeProjectID = Platform.environment['PINECONE_PROJECT_ID'];
+  if (pineconeProjectID == null) {
+    print('PINECONE_PROJECT_ID environment variable not set');
+    return;
+  }
+
+  // Create Pinecone client
+  PineconeClient pcClient = PineconeClient(
+    apiKey: pineconeApiKey,
+  );
+
+  // Create OpenAIEmbeddings client
   final openAIEmbeddings = OpenAIEmbeddings(
     apiKey: openAIApiKey,
     model: modelName,
   );
+
+  List<Document> documents = cli.populateDocuments(images);
   
-  // Convert all png images in the directory into OpenAI embeddings documents. 
-  // The file name is the text (word) and the full path is the metadata.
-  final List<Document> documents = [];
-  // use RegExp to grab the file name without the path
-  final RegExp regex = RegExp(r'[^/]*$');
-
-  for (var image in images) {
-    if (image.endsWith('.png')) {
-      final String fileName = regex.stringMatch(image)!;
-      if (fileName != '') {
-        documents.add(Document(pageContent: fileName, metadata: {'path': image}));
+  cli.convertDocumentsToEmbeddings(documents, openAIEmbeddings).then((docEmbeddings) { 
+      if (docEmbeddings.length != documents.length) {
+        print('Error: ${docEmbeddings.length} embeddings returned for ${documents.length} documents');
+        return;
       }
-    }
-    print(documents);
-  }
-
-  openAIEmbeddings.embedDocuments(documents);
+      cli.checkPineConeIndex(pcClient, pineconeEnv, pcIndex, docEmbeddings).then((indexes) {
+        if (indexes.contains(pcIndex)) {
+          print('Index $pcIndex already exists');
+          cli.upsertEmbeddings(pcClient, pineconeEnv, pcIndex, pineconeProjectID, documents, docEmbeddings).then((response) {
+              print('Upsert response: $response');
+          });
+        } else {
+          print('Creating index $pcIndex');
+          pcClient.createIndex(
+              environment: pineconeEnv,
+              request: CreateIndexRequest(name: pcIndex, dimension: docEmbeddings.first.length, metric: SearchMetric.cosine,
+            ),
+          ).then((response) {
+            // Pause a few seconds to allow the index to be created
+            sleep(Duration(seconds: 5));
+            cli.upsertEmbeddings(pcClient, pineconeEnv, pcIndex, pineconeProjectID, documents, docEmbeddings).then((response) {
+              print('Upsert response: $response');
+            });
+          });
+        }
+      }
+    );
+  });
 }
--- db/cli/lib/cli.dart
+++ db/cli/lib/cli.dart
@@ -1,5 +1,9 @@
 import 'dart:io';
+import 'package:langchain/langchain.dart' show Document;
+import 'package:langchain_openai/langchain_openai.dart' show OpenAIEmbeddings;
+import 'package:pinecone/pinecone.dart';
 
+// Get all images in the directory
 List<String> getImages(String path) {
   final dir = Directory(path);
   List<String> files = [];
@@ -13,3 +17,67 @@ List<String> getImages(String path) {
 
   return files;
 }
+
+// Populate the documents List with the image data
+List<Document> populateDocuments(List<String> images) {
+  final List<Document> documents = [];
+  // use RegExp to grab the file name without the path
+  final RegExp regex = RegExp(r'[^/]*$');
+
+  for (var image in images) {
+    if (image.endsWith('.png')) {
+      final String fileName = regex.stringMatch(image)!;
+      if (fileName != '') {
+        documents.add(Document(pageContent: fileName, metadata: {'path': image}));
+        print("Adding $fileName");
+      }
+    }
+  }
+
+  return documents;
+}
+
+// Convert the documents into OpenAI embeddings
+Future<List<List<double>>> convertDocumentsToEmbeddings(List<Document> documents, OpenAIEmbeddings openAIEmbeddings) {
+  return openAIEmbeddings.embedDocuments(documents).then((embeddings) { 
+      print("Embeddedings for ${embeddings.length} documents");
+      return embeddings;
+    } 
+  ).catchError((error) => print(error));
+}
+
+// Check Pinecone Indexes, creating the index if it doesn't exist. Return the projectID.
+Future<List<String>> checkPineConeIndex(
+    PineconeClient pcClient, 
+    String pineconeEnv, 
+    String pcIndex, 
+    List<List<double>> docEmbeddings) {
+  return pcClient.listIndexes(environment: pineconeEnv);
+}
+
+// Upsert the vectors into the Pinecone index
+Future<UpsertResponse> upsertEmbeddings(
+    PineconeClient pcClient, 
+    String pineconeEnv, 
+    String pcIndex, 
+    String pineconeProjectID,
+    List<Document> documents, 
+    List<List<double>> docEmbeddings,) {
+  return pcClient.upsertVectors(
+    indexName: pcIndex, 
+    projectId: pineconeProjectID, 
+    environment: pineconeEnv, 
+    request: UpsertRequest(
+      namespace: "openaac-images",
+        vectors: [
+          for (var i = 0; i < docEmbeddings.length; i++)
+            Vector(
+              id: 'img-$i',
+              values: docEmbeddings[i],
+              metadata: {'path': documents[i].metadata['path']},
+            ),
+      ],
+    )
+  );
+}
+

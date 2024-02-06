db7ce583bade9c1cbf9bf1af1ca4e61a994b2147
RonanOD <ronanodriscoll@gmail.com>
2024-02-05 21:47:11 -0400

    All images are cached now

--- app/open_aac/lib/ai.dart
+++ app/open_aac/lib/ai.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:image/image.dart' as img;
 
 const String modelName = 'text-embedding-ada-002';
@@ -10,7 +10,7 @@ const String pcIndex   = 'openaac-embeddings';
 const String namespace = 'openaac-images';
 const String blankTilePath = 'images/_app/blank.png';
 const double vectorMatchThreshold = 0.78;
-const String imageCachePrefix = "images/";
+const String imageCachePrefix = "images/openai/";
 
 class Mapping {
   final String word;
@@ -54,13 +54,9 @@ Future<List<Mapping>> lookupSupabase(String text) async {
             } else {
               var imagePath = match['path'];
               print("word $word => $imagePath");
-              final imageBytes = await sbClient.storage.from('images').download(imagePath);
-              /*final tempDir = await getTemporaryDirectory();
-              File file = await File('${tempDir.path}/image.png').create();
-              file.writeAsBytesSync(imageBytes);
-              mapping.imagePath = '${tempDir.path}/image.png';*/
+              final imageBytes = await _getFileFromCacheOrDownload(sbClient, imagePath);  
               mapping = Mapping(word, imagePath, false);
-              mapping.generatedImage = imageBytes;
+              mapping.generatedImage = imageBytes!;
             }
           } else {
             print("No match for $word. Attempting image generation.");
@@ -77,36 +73,52 @@ Future<List<Mapping>> lookupSupabase(String text) async {
   return mappings;
 }
 
+Future<Uint8List?> _getFileFromCacheOrDownload(SupabaseClient client, String imagePath, 
+    {bool generateImage = false, String word = ""}) async {
+  Uint8List? imageBytes;
+  var cacheFile = await DefaultCacheManager().getFileFromCache(imagePath);
+  if (cacheFile == null) {
+    if (generateImage) {
+      final response = await client.functions.invoke("generateImage", body: {'word': word});
+      String b64Json = response.data;
+
+      final base64Decoder = base64.decoder;
+      final decodedBytes = base64Decoder.convert(b64Json);
+      if (decodedBytes.isNotEmpty) {
+        img.Image? rawImage = img.decodeImage(decodedBytes);
+        img.Image resized = img.copyResize(rawImage!, width: 144, height: 144);
+        imageBytes = img.encodePng(resized);
+      } else {
+        return null;
+      }
+    } else {
+      imageBytes = await client.storage.from('images').download(imagePath);
+    }
+    // Put the image file in the cache for the next time.
+    await DefaultCacheManager().putFile(
+      imagePath,
+      imageBytes,
+      fileExtension: "png",
+      eTag: imagePath,
+      key: imagePath,
+      maxAge: Duration(days: 50),
+    );
+  } else {
+    imageBytes = cacheFile.file.readAsBytesSync();
+  }
+  return imageBytes;
+}
+
 // Use edge function to generate an image as there isn't a close enough vector match.
 // Cache generated images in local storage to keep costs down
 Future<Mapping> _generateImage(String word) async {
   Mapping mapping;
-  String? b64Json;
-  String cacheKey = imageCachePrefix + word;
+  final imagePath = imageCachePrefix + word;
+  
+  final resizedData = await _getFileFromCacheOrDownload(Supabase.instance.client, imagePath, 
+                                                generateImage: true, word: word);
 
-  // Access Supabase client
-  final sbClient = Supabase.instance.client;
-
-  // Check Shared Preferences for cached image data
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  final cachedB64Json = prefs.getString(cacheKey);
-
-  if (cachedB64Json == null) { // Call out to OpenAI to generate the image
-    final response = await sbClient.functions.invoke("generateImage", body: {'word': word});
-    print("word $word => Status: ${response.status} Initial: ${response.data}");
-    
-    b64Json = response.data;
-    prefs.setString(cacheKey, b64Json!); // Save to cache for future use
-  } else {
-    b64Json = cachedB64Json;
-  }
-
-  final base64Decoder = base64.decoder;
-  final decodedBytes = base64Decoder.convert(b64Json);
-  if (decodedBytes.isNotEmpty) {
-    img.Image? rawImage = img.decodeImage(decodedBytes);
-    img.Image resized = img.copyResize(rawImage!, width: 144, height: 144);
-    final resizedData = img.encodePng(resized);
+  if (resizedData != null) {
     mapping = Mapping(word, null, true);
     mapping.generatedImage = resizedData;
   } else {
